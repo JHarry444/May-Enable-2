14f0a00ffacce65ee8fbe30bccf2f31648986c2e
jordan <jharrison1@qa.com>
2022-07-11 11:05:15 +0100

    init commit

---
+++ Event-Handling/index.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const myInput = document.querySelector("input#myInput");
+const output = document.querySelector("#output");
+
+const writeInput = (event) => {
+    event.preventDefault(); // disables default handling for this event
+    const newInput = document.createElement("p");
+    newInput.innerText = myInput.value; // inputs use .value instead of .innerText
+    output.appendChild(newInput);
+
+    myInput.value = "";
+    myInput.focus();
+}
+
+const milesInput = document.querySelector("#milesInput");
+const kmsInput = document.querySelector("#kmsInput");
+
+const convertToKMs = () => {
+    const miles = parseFloat(milesInput.value);
+    const kms = miles * 1.6;
+    kmsInput.value = kms;
+}
+
+const convertToMiles = (event) => {
+    console.log("EVENT:", event);
+    const kms = parseFloat(event.target.value);
+    const miles = kms / 1.6;
+    milesInput.value = miles;
+}
